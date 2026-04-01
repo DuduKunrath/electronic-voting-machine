41c0e231c8cda18058bb14f41bd7c52b1158581f
Eduardo Kunrath <kunratheduardo@gmail.com>
2026-04-01 12:25:54 -0300

    feat: implementação inicial da urna eletrônica (HTML, CSS, JS)

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "38111",
+        nome: "Fulano de tal",
+        partido: "ABC",
+        fotos: [{ URL: "38111.jpg", legenda: "Vereador" }],
+      },
+      {
+        numero: "77222",
+        nome: "Beltrano da Silva",
+        partido: "DEFG",
+        fotos: [{ URL: "77222.jpg", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "99",
+        nome: "Ciclano",
+        partido: "ABC",
+        vice: "Cic",
+        fotos: [
+          { URL: "99.jpg", legenda: "Prefeito" },
+          { URL: "99_2.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: "84",
+        nome: "Zulano",
+        partido: "QWERTY",
+        vice: "Zul",
+        fotos: [
+          { URL: "84.jpg", legenda: "Prefeito" },
+          { URL: "84_2.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
